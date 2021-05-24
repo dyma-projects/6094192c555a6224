dcd7877694e7ad8bf4b98332b3a6c034dde3ccd6
NoodsGit <opendoorscomputing@gmail.com>
2021-05-24 18:45:56 +0200

    exercise 2 ok ? utilisation event keyup

--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  OnInit,
-  AfterViewInit,
-  ElementRef,
-  ViewChild,
-  Input,
-} from "@angular/core";
+import { Component, OnInit, ElementRef, ViewChild, Input } from "@angular/core";
 import {} from "@angular/core";
 
 @Component({
@@ -13,23 +6,17 @@ import {} from "@angular/core";
   templateUrl: "./exercice2.component.html",
   styleUrls: ["./exercice2.component.css"],
 })
-export class Exercice2Component implements OnInit, AfterViewInit {
+export class Exercice2Component implements OnInit {
+  @Input() inputText: string;
   @ViewChild("inputRef", { static: false }) el: ElementRef<HTMLInputElement>;
   public valeur: string;
 
   constructor() {}
   ngOnInit() {}
 
-  ngAfterViewInit(): void {
-    console.log("el: ", this.el);
-    // this.valeur = this.el.nativeElement.value;
-  }
-
-  // Je n'ai pas réussi à mettre à jour 'valeur' sans une trichérie, comme le bouton
-  // J'ai essayé les hooks, mais rien n'a marché
+  // Je n'ai pas réussi à mettre à jour 'valeur' sans faire la méthode applyChange() avec event keyup
 
   applyChange() {
-    console.log("el2: ", this.el);
     this.valeur = this.el.nativeElement.value;
   }
 }
